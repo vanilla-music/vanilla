bc1f5877306a45044c717fe8816e3e98cf492c89
Christopher Eby <kreed@kreed.org>
2011-10-25 17:41:51 -0500

    Use ant to create docs

--- doc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sdk=`grep sdk.dir= local.properties`
-sdk=${sdk#sdk.dir=}
-target=`grep target= project.properties`
-target=${target#target=}
-exec javadoc -classpath "$sdk/platforms/$target/android.jar" -d doc -sourcepath src -private org.kreed.vanilla
