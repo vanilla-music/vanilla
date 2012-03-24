e6d74b6a1b67cf5ce1ee53be3aaaaf23f3a7810b
Christopher Eby <kreed@kreed.org>
2012-03-24 01:44:44 -0500

    Split preferences into multiple screens; add about page

--- src/org/kreed/vanilla/PreferencesActivity.java
+++ src/org/kreed/vanilla/PreferencesActivity.java
@@ -22,11 +22,21 @@
 
 package org.kreed.vanilla;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceGroup;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewFragment;
+import java.util.List;
 
 /**
  * The preferences activity in which one can change application preferences.
@@ -35,16 +45,21 @@ public class PreferencesActivity extends PreferenceActivity {
 	/**
 	 * Initialize the activity, loading the preference specifications.
 	 */
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		addPreferencesFromResource(R.xml.preferences);
-
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			PreferenceGroup library = (PreferenceGroup)findPreference("library");
-			library.removePreference(findPreference("controls_in_selector"));
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+			addPreferencesFromResource(R.xml.preferences);
 		}
+	}
+
+	@TargetApi(11)
+	@Override
+	public void onBuildHeaders(List<Header> target)
+	{
+		loadHeadersFromResource(R.xml.preference_headers, target);
 	}
 
 	@Override
@@ -55,6 +70,156 @@ public class PreferencesActivity extends PreferenceActivity {
 			return true;
 		} else {
 			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public static class AudioActivity extends PreferenceActivity {
+		@SuppressWarnings("deprecation")
+		@Override
+		public void onCreate(Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preference_audio);
+		}
+	}
+
+	@TargetApi(11)
+	public static class AudioFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preference_audio);
+		}
+	}
+
+	public static class PlaybackActivity extends PreferenceActivity {
+		@SuppressWarnings("deprecation")
+		@Override
+		public void onCreate(Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preference_playback);
+		}
+	}
+
+	@TargetApi(11)
+	public static class PlaybackFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preference_playback);
+		}
+	}
+
+	public static class LibraryActivity extends PreferenceActivity {
+		@SuppressWarnings("deprecation")
+		@Override
+		public void onCreate(Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preference_library);
+		}
+	}
+
+	@TargetApi(11)
+	public static class LibraryFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preference_library);
+			PreferenceGroup group = getPreferenceScreen();
+			group.removePreference(group.findPreference("controls_in_selector"));
+		}
+	}
+
+	public static class NotificationsActivity extends PreferenceActivity {
+		@SuppressWarnings("deprecation")
+		@Override
+		public void onCreate(Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preference_notifications);
+		}
+	}
+
+	@TargetApi(11)
+	public static class NotificationsFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preference_notifications);
+		}
+	}
+
+	public static class ShakeActivity extends PreferenceActivity {
+		@SuppressWarnings("deprecation")
+		@Override
+		public void onCreate(Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preference_shake);
+		}
+	}
+
+	@TargetApi(11)
+	public static class ShakeFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preference_shake);
+		}
+	}
+
+	public static class MiscActivity extends PreferenceActivity {
+		@SuppressWarnings("deprecation")
+		@Override
+		public void onCreate(Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preference_misc);
+		}
+	}
+
+	@TargetApi(11)
+	public static class MiscFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preference_misc);
+		}
+	}
+
+	public static class AboutActivity extends Activity {
+		@Override
+		public void onCreate(Bundle state)
+		{
+			super.onCreate(state);
+
+			WebView view = new WebView(this);
+			view.getSettings().setJavaScriptEnabled(true);
+			view.loadUrl("file:///android_asset/about.html");
+			view.setBackgroundColor(Color.TRANSPARENT);
+			setContentView(view);
+		}
+	}
+
+	@TargetApi(11)
+	public static class AboutFragment extends WebViewFragment {
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			WebView view = (WebView)super.onCreateView(inflater, container, savedInstanceState);
+			view.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+			view.getSettings().setJavaScriptEnabled(true);
+			view.loadUrl("file:///android_asset/about.html");
+			view.setBackgroundColor(Color.TRANSPARENT);
+			return view;
 		}
 	}
 }
