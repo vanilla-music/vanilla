42fe31d33de24c188e1b5dacd8d68253ac907845
Nico Alt <nicoalt@posteo.org>
2015-04-07 20:23:36 +0200

    preserve the project structure

--- generate-pngs.sh
+++ generate-pngs.sh
@@ -12,7 +12,7 @@
 
 gen() {
 	name=`basename "$1" .svgz`
-	png="app/src/main/res/drawable-$2/$name.png"
+	png="res/drawable-$2/$name.png"
 	if [ "$1" -nt "$png" -o ! -e "$png" ]; then
 		inkscape --without-gui --export-area-page --export-dpi=$3 --export-png="$png" $1
 		convert -strip "$png" "$png" # strip comments and other metadata
