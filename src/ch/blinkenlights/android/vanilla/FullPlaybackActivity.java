71df5071e805c693189647f9e6cb2a08928ba2dd
Adrian Ulrich <adrian@blinkenlights.ch>
2015-04-22 21:05:30 +0200

    replace clear_queue with dequeue_rest

--- src/ch/blinkenlights/android/vanilla/FullPlaybackActivity.java
+++ src/ch/blinkenlights/android/vanilla/FullPlaybackActivity.java
@@ -360,7 +360,7 @@ public class FullPlaybackActivity extends PlaybackActivity
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
 		super.onCreateOptionsMenu(menu);
-		menu.add(0, MENU_CLEAR_QUEUE, 0, R.string.clear_queue).setIcon(R.drawable.ic_menu_close_clear_cancel);
+		menu.add(0, MENU_CLEAR_QUEUE, 0, R.string.dequeue_rest).setIcon(R.drawable.ic_menu_close_clear_cancel);
 		menu.add(0, MENU_DELETE, 0, R.string.delete);
 		menu.add(0, MENU_ENQUEUE_ALBUM, 0, R.string.enqueue_current_album).setIcon(R.drawable.ic_menu_add);
 		menu.add(0, MENU_ENQUEUE_ARTIST, 0, R.string.enqueue_current_artist).setIcon(R.drawable.ic_menu_add);
