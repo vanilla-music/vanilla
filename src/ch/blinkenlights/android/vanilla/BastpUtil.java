8fbdc201e92c7acecc1c58e67e3a2b629b260abe
Adrian Ulrich <adrian@blinkenlights.ch>
2016-05-29 18:29:03 +0200

    normalize from R128 -> rgain

--- src/ch/blinkenlights/android/vanilla/BastpUtil.java
+++ src/ch/blinkenlights/android/vanilla/BastpUtil.java
@@ -29,7 +29,6 @@ public class BastpUtil {
 	private RGLruCache rgCache;
 	/**
 	 * What we return & cache
-	 * These are normalized to conform to EBU R128
 	 */
 	public class GainValues {
 		public float base;
@@ -76,17 +75,17 @@ public class BastpUtil {
 
 		// normal replay gain, add 5dB difference
 		if(tags.containsKey("REPLAYGAIN_TRACK_GAIN"))
-			gv.track = getFloatFromString((String)((Vector)tags.get("REPLAYGAIN_TRACK_GAIN")).get(0)) - 5.0f;
+			gv.track = getFloatFromString((String)((Vector)tags.get("REPLAYGAIN_TRACK_GAIN")).get(0));
 		if(tags.containsKey("REPLAYGAIN_ALBUM_GAIN"))
-			gv.album = getFloatFromString((String)((Vector)tags.get("REPLAYGAIN_ALBUM_GAIN")).get(0)) - 5.0f;
+			gv.album = getFloatFromString((String)((Vector)tags.get("REPLAYGAIN_ALBUM_GAIN")).get(0));
 
-		// already R128, most likely OPUS
+		// likely OPUS
 		if(tags.containsKey("R128_BASTP_BASE_GAIN"))
-			gv.base = getFloatFromString((String)((Vector)tags.get("R128_BASTP_BASE_GAIN")).get(0)) / 256.0f;
+			gv.base = 0.0f + getFloatFromString((String)((Vector)tags.get("R128_BASTP_BASE_GAIN")).get(0)) / 256.0f;
 		if(tags.containsKey("R128_TRACK_GAIN"))
-			gv.track = getFloatFromString((String)((Vector)tags.get("R128_TRACK_GAIN")).get(0)) / 256.0f;
+			gv.track = 5.0f + getFloatFromString((String)((Vector)tags.get("R128_TRACK_GAIN")).get(0)) / 256.0f;
 		if(tags.containsKey("R128_ALBUM_GAIN"))
-			gv.album = getFloatFromString((String)((Vector)tags.get("R128_ALBUM_GAIN")).get(0)) / 256.0f;
+			gv.album = 5.0f + getFloatFromString((String)((Vector)tags.get("R128_ALBUM_GAIN")).get(0)) / 256.0f;
 
 		return gv;
 	}
