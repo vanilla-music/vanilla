3a421e7a397e990686b6e7258613b967cf6ca739
Adrian Ulrich <adrian@blinkenlights.ch>
2017-05-31 18:37:59 +0200

    Add missing cursor != null check.
    Samsung seems to return a null cursor instead of throwing a SecurityException.

--- src/ch/blinkenlights/android/vanilla/MediaUtils.java
+++ src/ch/blinkenlights/android/vanilla/MediaUtils.java
@@ -590,11 +590,13 @@ public class MediaUtils {
 		String[] projection = new String[]{ MediaStore.Audio.Media._ID, MediaStore.Audio.Media.ALBUM_ID, MediaStore.Audio.Media.ARTIST_ID };
 		try {
 			Cursor cursor = context.getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, MediaStore.Audio.Media.DATA+"=?", new String[] { song.path }, null);
-			if (cursor.moveToFirst()) {
-				for (int i=0; i<result.length; i++)
-					result[i] = cursor.getLong(i);
+			if (cursor != null) {
+				if (cursor.moveToFirst()) {
+					for (int i=0; i<result.length; i++)
+						result[i] = cursor.getLong(i);
+				}
+				cursor.close();
 			}
-			cursor.close();
 		} catch (SecurityException e) {
 			Log.e("VanillaMusic", "Wowies: No permission to read EXTERNAL_CONTENT_URI for song "+song.path+": "+e);
 		}
