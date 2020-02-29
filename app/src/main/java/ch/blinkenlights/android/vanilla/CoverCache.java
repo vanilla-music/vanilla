5f11ca5d3db3c7e87b104a7213169db0b4628e76
Kilian Köppchen <kiliankoeppchen@googlemail.com>
2020-02-29 17:24:01 +0100

    Fix comments within CoverCache class.

--- app/src/main/java/ch/blinkenlights/android/vanilla/CoverCache.java
+++ app/src/main/java/ch/blinkenlights/android/vanilla/CoverCache.java
@@ -100,7 +100,7 @@ public class CoverCache {
 	/**
 	 * Returns a (possibly uncached) cover for the song - will return null if the song has no cover
 	 *
-	 * @param key The cache key to use for storing a generated cover
+	 * @param ctx The context to retrieve the bitmap from cache via external content uri
 	 * @param song The song used to identify the artwork to load
 	 * @return a bitmap or null if no artwork was found
 	 */
@@ -330,7 +330,7 @@ public class CoverCache {
 		 * Stores a bitmap in the disk cache, does not update existing objects
 		 *
 		 * @param key The cover key to use
-		 * @param Bitmap The bitmap to store
+		 * @param cover The cover to store as bitmap
 		 */
 		public void put(CoverKey key, Bitmap cover) {
 			SQLiteDatabase dbh = getWritableDatabase();
@@ -390,6 +390,7 @@ public class CoverCache {
 		 * Attempts to create a new bitmap object for given song.
 		 * Returns null if no cover art was found
 		 *
+		 * @param ctx The context to read the external content uri of the given song
 		 * @param song the function will search for artwork of this object
 		 * @param maxPxCount the maximum amount of pixels to return (30*30 = 900)
 		 */
